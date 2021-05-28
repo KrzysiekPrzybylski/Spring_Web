016ac4e63203513a9266b1f8d52b00ea664e3dc2
Krzysiek Przybylski <krzy.przybylski@gmail.com>
2021-05-29 00:59:46 +0200

    29.1. Stosowanie wzorca fasady w projekcie

--- src/main/java/com/crud/tasks/domain/TrelloList.java
+++ src/main/java/com/crud/tasks/domain/TrelloList.java
@@ -4,21 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
-public class TrelloListDto {
+public class TrelloList {
 
-    @JsonProperty("name")
     private String name;
-
-    @JsonProperty("id")
     private String id;
-
-    @JsonProperty("closed")
     private boolean isClosed;
 
 }
